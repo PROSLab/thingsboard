5c4a88925a25ba7a920724efd9d6d177cc81bf55
Matteo Romagnoli <matteo02.romagnoli@studenti.unicam.it>
2023-03-14 18:40:08 +0100

    [CONFIG/FEATURE] Added Threejs and types; implemented raw scene

--- ui-ngx/src/app/modules/home/components/widget/threed-view-widget/threed-view-widget.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/threed-view-widget/threed-view-widget.component.ts
@@ -14,33 +14,69 @@
 /// limitations under the License.
 ///
 
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { AppState } from '@app/core/core.state';
 import { WidgetContext } from '@app/modules/home/models/widget-component.models';
 import { PageComponent } from '@app/shared/public-api';
 import { Store } from '@ngrx/store';
+import * as THREE from 'three';
 
 @Component({
   selector: 'tb-threed-view-widget',
   templateUrl: './threed-view-widget.component.html',
   styleUrls: ['./threed-view-widget.component.scss']
 })
-export class ThreedViewWidgetComponent extends PageComponent implements OnInit {
+export class ThreedViewWidgetComponent extends PageComponent implements OnInit, AfterViewInit {
 
   //settings: MarkdownWidgetSettings;
 
   @Input()
   ctx: WidgetContext;
 
+  @ViewChild('rendererContainer') rendererContainer: ElementRef;
+
+  renderer = new THREE.WebGLRenderer();
+  scene = null;
+  camera = null;
+  mesh = null;
+
   constructor(protected store: Store<AppState>,
     protected cd: ChangeDetectorRef) {
     super(store);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    this.camera.position.z = 1000;
+
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.mesh);
   }
 
   ngOnInit(): void {
     this.ctx.$scope.threedViewWidget = this;
     //this.settings = this.ctx.settings;
     //this.qrCodeTextFunction = this.settings.useQrCodeTextFunction ? parseFunction(this.settings.qrCodeTextFunction, ['data']) : null;
+  }
+
+  ngAfterViewInit() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.animate();
+  }
+
+  animate() {
+    window.requestAnimationFrame(() => this.animate());
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  public onResize() : void {
+    console.log("onresize!!");
   }
 
 }
